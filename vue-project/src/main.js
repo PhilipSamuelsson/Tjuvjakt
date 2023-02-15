9e9b2588e15a91c15d41469f2b6e56b4140dee83
PhilipSamuelsson <philip.samuelsson@iths.se>
2023-02-15 10:35:29 +0100

    Installing bootstrap and adding a simple navbar

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "bootstrap/dist/css/bootstrap.css";
 
-import "./assets/main.css";
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
 
 const app = createApp(App);
 
 app.use(router);
-
+app.use(bootstrap);
 app.mount("#app");
