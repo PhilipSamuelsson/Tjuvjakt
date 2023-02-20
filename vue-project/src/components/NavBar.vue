ba4a30441ddc26ea83e995a49cd564a0862fe0d6
PhilipSamuelsson <philip.samuelsson@iths.se>
2023-02-20 12:34:21 +0100

    Product-Card + Router Links

--- vue-project/src/components/NavBar.vue
+++ vue-project/src/components/NavBar.vue
@@ -30,21 +30,15 @@
               ><RouterLink to="/products">Produkter</RouterLink></a
             >
           </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
+          <li class="nav-item">
+            <a class="nav-link" href="#"
+              ><RouterLink to="/profile">profil</RouterLink></a
             >
-              Dropdown link
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"
+              ><RouterLink to="/checkout">Kassa</RouterLink></a
+            >
           </li>
         </ul>
       </div>
