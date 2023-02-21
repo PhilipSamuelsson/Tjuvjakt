5aa81fe73cda05558d4db4af00abe9e88fb04d24
ShahadAdil <71069861+ShahadAdil@users.noreply.github.com>
2023-02-21 20:27:33 +0100

    NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,11 @@
 <template>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<!-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300&display=swap" rel="stylesheet"> -->
 
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">Tjuvgods</a>
       <button
         class="navbar-toggler"
         type="button"
@@ -18,27 +21,27 @@
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">
-              <RouterLink to="/">Home</RouterLink></a
+              <RouterLink to="/">Hem</RouterLink></a
             >
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">
-              <RouterLink to="/about">Om oss</RouterLink></a
+              <RouterLink to="/about">Produkt</RouterLink></a
             >
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#"
-              ><RouterLink to="/products">Produkter</RouterLink></a
+              ><RouterLink to="/products">Mitt konto</RouterLink></a
             >
           </li>
-          <li class="nav-item">
+          <!-- <li class="nav-item">
             <a class="nav-link" href="#"
               ><RouterLink to="/profile">profil</RouterLink></a
             >
-          </li>
+          </li> -->
           <li class="nav-item">
             <a class="nav-link" href="#"
-              ><RouterLink to="/checkout">Kassa</RouterLink></a
+              ><RouterLink to="/checkout"><img class="Kassa" src="../assets/img/shopping-cart-icon-1.png"></RouterLink></a
             >
           </li>
         </ul>
@@ -49,7 +52,12 @@
 
 
 <style>
+
+  @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@300&display=swap');
+
+
 nav {
+  font-family: 'Outfit', sans-serif;
   background: white;
   height: 40px;
   width: 100%;
@@ -57,7 +65,7 @@ nav {
 
 nav ul li {
   display: inline-block;
-  margin: 0 20px;
+  margin: 0px 20px;
 }
 
 nav ul li a {
@@ -70,6 +78,14 @@ nav ul li a:hover {
 }
 
 
+nav[class="navbar navbar-expand-lg bg-body-tertiary"]{
+  /* font-family: 'Outfit', sans-serif; */
+  font-family: 'Turret Road', cursive;
+  background-color: white;
+}
 
-
+.Kassa{
+  height: 20px;
+  width: 20px;
+}
 </style>
