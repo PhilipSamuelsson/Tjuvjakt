f403b21facf28378c8ed36077104447f398d6b8f
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-02-21 11:31:01 +0100

    lagt grunden för Footer

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <footer>
+    <h4 class="footer-header">Quick Links</h4>
+    <div class="footer-links">
+      <RouterLink to="#">Terms of Service</RouterLink>
+      <RouterLink to="#">privacy Policies</RouterLink>
+      <RouterLink to="#">(No) Refund Policy</RouterLink>
+      <RouterLink to="#">(No) Contact Policy</RouterLink>
+    </div>
+    <div class="footer-icon">
+      <i class="fa fa-facebook" aria-hidden="true"></i>
+      <i class="fa fa-twitter" aria-hidden="true"></i>
+      <i class="fa fa-reddit" aria-hidden="true"></i>
+      <i class="fa fa-snapchat-ghost" aria-hidden="true"></i>
+      <i class="fa fa-paypal" aria-hidden="true"></i>
+    </div>
+  </footer>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+footer {
+  position: absolute;
+  bottom: 0;
+}
+</style>
