f56f1dcbadc7ddae8a4082f717e3fd2f292bc86b
ShahadAdil <71069861+ShahadAdil@users.noreply.github.com>
2023-02-21 21:16:23 +0100

    NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<!-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300&display=swap" rel="stylesheet"> -->
+   <!-- <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300&display=swap" rel="stylesheet"/> -->
 
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Tjuvgods</a>
+      <a class="navbar-brand" href="#"><img class="img-logo" src="../assets/img/tjuvgods_logo.png"></a>
       <button
         class="navbar-toggler"
         type="button"
@@ -13,36 +13,36 @@
         data-bs-target="#navbarNavDropdown"
         aria-controls="navbarNavDropdown"
         aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+        aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
+
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">
-              <RouterLink to="/">Hem</RouterLink></a
-            >
+              <RouterLink to="/">Hem</RouterLink></a>
           </li>
+
           <li class="nav-item">
             <a class="nav-link" href="#">
-              <RouterLink to="/about">Produkt</RouterLink></a
-            >
+              <RouterLink to="/about">Produkt</RouterLink></a>
           </li>
+
           <li class="nav-item">
-            <a class="nav-link" href="#"
-              ><RouterLink to="/products">Mitt konto</RouterLink></a
-            >
+            <a class="nav-link" href="#">
+            <RouterLink to="/products">Mitt konto</RouterLink></a>
           </li>
+
           <!-- <li class="nav-item">
             <a class="nav-link" href="#"
               ><RouterLink to="/profile">profil</RouterLink></a
             >
           </li> -->
+
           <li class="nav-item">
-            <a class="nav-link" href="#"
-              ><RouterLink to="/checkout"><img class="Kassa" src="../assets/img/shopping-cart-icon-1.png"></RouterLink></a
-            >
+            <a class="nav-link" href="#"> <RouterLink to="/checkout">
+             <img class="Kassa" src="../assets/img/shopping-cart-icon-1.png" /></RouterLink></a>
           </li>
         </ul>
       </div>
@@ -50,14 +50,17 @@
   </nav>
 </template>
 
-
 <style>
 
-  @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@300&display=swap');
-
+nav[class="navbar navbar-expand-lg bg-body-tertiary"] {
+  font-family: "Turret Road", cursive;
+  /* font-family: Outfit; */
+  background-color: white;
+  font-weight: bold;
+}
 
 nav {
-  font-family: 'Outfit', sans-serif;
+  font-family: "Outfit", sans-serif;
   background: white;
   height: 40px;
   width: 100%;
@@ -65,7 +68,7 @@ nav {
 
 nav ul li {
   display: inline-block;
-  margin: 0px 20px;
+  margin: 0 20px;
 }
 
 nav ul li a {
@@ -77,14 +80,11 @@ nav ul li a:hover {
   color: black;
 }
 
-
-nav[class="navbar navbar-expand-lg bg-body-tertiary"]{
-  /* font-family: 'Outfit', sans-serif; */
-  font-family: 'Turret Road', cursive;
-  background-color: white;
+.img-logo{
+  height: 25px;
+  width: 100px;
 }
-
-.Kassa{
+.Kassa {
   height: 20px;
   width: 20px;
 }
