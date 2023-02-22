89e1b42862c8f50969c81e06a95df05c1effbd5e
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-02-22 10:51:59 +0100

    Summary

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,38 +5,29 @@
 
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"
-        ><img class="img-logo" src="../assets/img/tjuvgods_logo.png" alt="Logo"
-      /></a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <a class="navbar-brand" href="#"><img class="img-logo" src="../assets/img/tjuvgods_logo.png" alt="Logo" /></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">
-              <RouterLink to="/">Hem</RouterLink></a
-            >
+              <RouterLink to="/">Hem</RouterLink>
+            </a>
           </li>
 
           <li class="nav-item">
             <a class="nav-link" href="#">
-              <RouterLink to="/products">Produkt</RouterLink></a
-            >
+              <RouterLink to="/about">Produkt</RouterLink>
+            </a>
           </li>
 
           <li class="nav-item">
             <a class="nav-link" href="#">
-              <RouterLink to="/profile">Mitt konto</RouterLink></a
-            >
+              <RouterLink to="/products">Mitt konto</RouterLink>
+            </a>
           </li>
 
           <!-- <li class="nav-item">
@@ -48,10 +39,9 @@
           <li class="nav-item">
             <a class="nav-link" href="#">
               <RouterLink to="/checkout">
-                <img
-                  class="Kassa"
-                  src="../assets/img/shopping-cart-icon-1.png" /></RouterLink
-            ></a>
+                <img class="Kassa" src="../assets/img/shopping-cart-icon-1.png" />
+              </RouterLink>
+            </a>
           </li>
         </ul>
       </div>
@@ -93,6 +83,7 @@ nav ul li a:hover {
   height: 25px;
   width: 147px;
 }
+
 .Kassa {
   height: 28px;
   width: 39px;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import '/main.css'
+import "/main.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
