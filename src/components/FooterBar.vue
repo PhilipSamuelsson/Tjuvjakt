e9b617e2158086edadbc24052e415f982d8f5b91
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-02-24 17:52:36 +0100

    Fixat styling på responsiv FooterBar. Dax att fixa toggle-funktionalitet

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -6,10 +6,24 @@ import { RouterLink } from "vue-router";
   <footer class="footerbar">
     <h4 class="footer-header">Quick Links</h4>
     <div class="footer-links">
-      <RouterLink to="#">Terms of Service</RouterLink>
-      <RouterLink to="#">Privacy Policies</RouterLink>
-      <RouterLink to="#">(No) Refund Policy</RouterLink>
-      <RouterLink to="#">(No) Contact Policy</RouterLink>
+      <div class="mobile">
+        <p>Villkor</p>
+        <i class="fa-solid fa-plus" id="togglare"></i>
+        <i class="fa-solid fa-minus" id="togglare"></i>
+      </div>
+      <div class="villkor">
+        <RouterLink to="#">Terms of Service</RouterLink>
+        <RouterLink to="#">Privacy Policies</RouterLink>
+      </div>
+      <div class="mobile">
+        <p>Kontakt</p>
+        <i class="fa-solid fa-plus" id="togglare"></i>
+        <i class="fa-solid fa-minus" id="togglare"></i>
+      </div>
+      <div class="kontakt">
+        <RouterLink to="#">(No) Refund Policy</RouterLink>
+        <RouterLink to="#">(No) Contact Policy</RouterLink>
+      </div>
     </div>
     <div class="footer-icon">
       <i class="fa-brands fa-youtube"></i>
@@ -32,6 +46,11 @@ import { RouterLink } from "vue-router";
   width: 98vw;
   /* border-top: 2px solid black; */
   padding: auto 20px;
+  margin-top: 17vh;
+}
+
+.mobile {
+  display: none;
 }
 
 .footer-header {
@@ -45,11 +64,18 @@ import { RouterLink } from "vue-router";
   justify-content: center;
 }
 
-.footer-links * {
-  margin: 20px 55px 10px;
-  font-size: 24px;
+.villkor,
+.kontakt {
+  margin: 30px auto;
+}
+
+.footer-links .villkor a,
+.kontakt a {
+  margin: auto 25px;
+  font-size: 1.5rem;
   text-decoration: none;
   color: black;
+  text-align: center;
 }
 
 .footer-icon {
@@ -60,5 +86,43 @@ import { RouterLink } from "vue-router";
 .footer-icon * {
   margin: 12px 25px 10px;
   font-size: 2em;
+}
+
+@media (max-width: 949px) {
+  .footer-header {
+    display: none;
+  }
+
+  .footer-links {
+    flex-direction: column;
+  }
+
+  .mobile {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: auto 15vw;
+    /* align-items: center; */
+  }
+
+  .footer-links a {
+    display: none;
+  }
+
+  .mobile * {
+    font-size: 24px;
+    font-weight: bold;
+    align-self: center;
+    padding: .2rem;
+  }
+
+  /* .mobile i {
+    align-self: center;
+  } */
+
+  .footer-links .mobile i.fa-solid.fa-minus {
+    display: none;
+  }
+
 }
 </style>
