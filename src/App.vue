8d3ba56d278c8d7548dbf2c968063355ea49304e
PhilipSamuelsson <philip.samuelsson@iths.se>
2023-03-01 20:33:59 +0100

    Profile

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import NavBar from "./components/NavBar.vue";
-import NavbarMobile from "./components/NavbarMobile.vue";
 </script>
 
 <template>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,4 +1,42 @@
 <!-- <script setup>
 </script> -->
 
-<template></template>
+<template>
+  <h2>Ditt konto</h2>
+  <div class="profileMenu">
+    <div class="profileBar">Beställningar</div>
+    <div class="profileBar">Returnera artikel</div>
+    <div class="profileBar">Adresser</div>
+    <div class="profileBar">Betalmetoder</div>
+    <div class="profileBar">Kundservice</div>
+    <div class="profileBar">Kundservice</div>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  font-weight: 700;
+  margin-left: 35px;
+  margin-top: 50px;
+  font-size: 2.5rem;
+}
+.profileBar {
+  border: 1px solid black;
+  margin-top: 30px;
+  width: 90%;
+  height: 50px;
+  padding: 10px;
+  font-size: 1.75rem;
+  display: flex;
+  align-items: center;
+  vertical-align: center;
+}
+
+.profileMenu {
+  border: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
