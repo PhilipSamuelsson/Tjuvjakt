8589bb9d8920b17e6c544c9398791a98c56701a0
chrillah <ch.ronnberg@gmail.com>
2023-03-01 20:25:32 +0100

    small changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -76,7 +76,7 @@ export default {
       <div class="navbar-nav ml-auto">
         <a @click="$store.commit('toggleCart')" class="nav-link cart" href="#">
           <img class="Kassa" src="../assets/img/shopping-cart-icon-1.png" />
-          <p class="cart-number" v-if="displays">{{ getCartItemCount }}</p>
+          <p class="cart-number" >{{ getCartItemCount }}</p>
         </a>
       </div>
     </div>
